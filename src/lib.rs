667e00b6cb7c4ea741dc0ba3e36c7a5a54d29f25
bonedaddy <boned4ddy@protonmail.com>
2022-11-20 14:17:52 -0800

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@ pub fn handle_anyix<'info>(
     for idx in 0..num_instructions {
         use solana_program::msg;
         let accounts =
-            &accounts[offset as usize..instruction_account_counts[idx as usize] as usize];
+            &accounts[offset as usize..instruction_account_counts[idx as usize] as usize + idx as usize];
         offset += instruction_account_counts[idx as usize];
         let program_account = &accounts[0];
         if program_id.eq(program_account.key) {
